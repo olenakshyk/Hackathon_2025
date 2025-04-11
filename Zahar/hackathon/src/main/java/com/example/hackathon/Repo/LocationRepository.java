c8e4ced3a7f8b9d83e830cbedc8c05328d90e39f
Zakharii Shturyn <93404523+Pro7it@users.noreply.github.com>
2025-04-11 17:04:29 +0300

    delete everything))))

--- Zahar/hackathon/src/main/java/com/example/hackathon/Repo/LocationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.hackathon.Repo;
-
-import com.example.hackathon.Entity.Location;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface LocationRepository extends JpaRepository<Location, Long>, JpaSpecificationExecutor<Location> {}
